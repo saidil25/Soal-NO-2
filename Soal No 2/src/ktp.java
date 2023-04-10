2e35777e54caf9e9e8d916ab637cffcbc775383c
saidil25 <110053003+saidil25@users.noreply.github.com>
2023-04-10 14:04:28 +0700

    Add files via upload

---
+++ Soal No 2/src/ktp.java
@@ -0,0 +1,13 @@
+public class ktp {
+ public String nama;
+ private String tgl;
+ public String telp;
+
+    public ktp(String nama, int tgl, int telp) {
+        this.nama = nama;
+        this.tgl = tgl;
+        this.telp = telp;
+
+
+    }
+}
